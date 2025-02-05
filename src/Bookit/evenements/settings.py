44c2ddb79c9de393fc4e17df3adce50fadbbaca9
Yatoute <mintoamayatoute@gmail.com>
2025-02-05 23:45:34 +0100

    configuration django

--- src/Bookit/evenements/settings.py
+++ src/Bookit/evenements/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ecr)cf@52dv#pr&cz49q_*+nre95t9te#5d*yt+$x635%)7dm-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
