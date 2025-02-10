064dcbc59272f26bda1144e62810456d7069b856
Yatoute <mintoamayatoute@gmail.com>
2025-02-10 18:22:05 +0100

    Creation de classes User, Event

--- src/Bookit/evenements/models.py
+++ src/Bookit/evenements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from model_user import User
 
+
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=512)
