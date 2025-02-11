8347d77653aec41d14e495ee3ac7b20b1acea63c
Yatoute <mintoamayatoute@gmail.com>
2025-02-11 23:27:35 +0100

    mise à jour du modèle Event

--- src/backend/auth/models.py
+++ src/backend/auth/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        
+        return (self.id)
--- src/backend/evenements/models.py
+++ src/backend/evenements/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from ..auth.models import User
 
 
 class Event(models.Model):
+
     id = models.AutoField(primary_key=True)
-    description = models.CharField(max_length=512)
+
+    description = models.CharField(
+        max_length=512,
+        help_text=_(
+            "Give some description to this even."
+        ))
+    
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    is_reserved  = models.BooleanField(
+        _("reserved"),
+        default=True, 
+        )
+    
+    def __str__(self):
+        
+        return(self.id)
