d609223c474b4d7910ef7ce0341cc70ab08aedd2
Yatoute <mintoamayatoute@gmail.com>
2025-02-13 08:30:01 +0100

    Initialisation d'un template react pour Vite

---
+++ src/frontend/src/api.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants"
+
+// Créer une instance de Axios 
+// pour rediriger les requêtes vers notre serveur django REST framework.
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+})
+
+
+// Un interceptor pour ajouter l'authorization token à chaque requête
+api.interceptors.request.use(
+    async (config) => {
+        const accessToken = localStorage.getItem(ACCESS_TOKEN);
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
+    }
+);
