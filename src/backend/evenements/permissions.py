fa0edfb924e1b0fdd9066ee2cdb16b37ccf8a693
Yatoute <mintoamayatoute@gmail.com>
2025-02-13 21:52:11 +0100

    update requirements.txt

--- src/backend/evenements/permissions.py
+++ src/backend/evenements/permissions.py
@@ -1,12 +1,15 @@
 from rest_framework.permissions import BasePermission
 
+
 class IsEventAdmin(BasePermission):
     """
     Permission personnalisée qui autorise uniquement les utilisateurs
-    appartenant au groupe 'event_admin' à accéder à la vue.
+    appartenant au groupe 'event_admin' à accéder à la vue .
     """
+
     def has_permission(self, request, view):
-        return request.user and request.user.groups.filter(name='event_admin').exists()
+        return request.user and request.user.groups.filter(name="event_admin").exists()
+
 
 class IsEventAdminAndOwner(IsEventAdmin):
     """
@@ -14,5 +17,6 @@ class IsEventAdminAndOwner(IsEventAdmin):
     appartenant au groupe 'event_admin' et qui sont propriétaires de l'événement
     à accéder à la vue.
     """
+
     def has_object_permission(self, request, view, event):
         return super().has_permission(request, view) and event.organizer == request.user
