e057b63d8dc927ef201211de1b5563bebed2afd5
Richard GOZAN <gozamegborichard14@gmail.com>
2025-02-13 20:02:34 +0100

    Création des modèles pour reservations, configurations de .vscode/settings.json et modification de .gitignore

--- src/backend/evenements/models.py
+++ src/backend/evenements/models.py
@@ -5,8 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Room(models.Model):
-    """
-    """
+    """ """
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64, unique=True)
@@ -14,12 +13,11 @@ class Room(models.Model):
 
     def __str__(self):
 
-        return (str(self.id))
+        return str(self.id)
 
 
 class Bus(models.Model):
-    """
-    """
+    """ """
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64, unique=True)
@@ -27,83 +25,78 @@ class Bus(models.Model):
 
     def __str__(self):
 
-        return (str(self.id))
+        return str(self.id)
 
 
 class Material(models.Model):
-    """
-    """
+    """ """
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64, unique=True)
     stock = models.IntegerField(validators=[MinValueValidator(0)])
 
     def __str__(self):
 
-        return (str(self.id))
+        return str(self.id)
 
 
 class Event(models.Model):
-    """
-    """
+    """ """
 
     id = models.AutoField(primary_key=True)
     description = models.CharField(
-        max_length=512,
-        help_text=_(
-            "Give some description to this even."
-        ))
+        max_length=512, help_text=_("Give some description to this even.")
+    )
     organizer = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_reserved  = models.BooleanField(
+    is_reserved = models.BooleanField(
         _("reserved"),
-        default=True, 
-        )
-    
+        default=True,
+    )
+
     class Meta:
         abstract = True
-        
-    def __str__(self):
-        return(str(self.id))
-    
 
-class EvenRoom(Event):
-    """
-    """
+    def __str__(self):
+        return str(self.id)
+
+
+class EventRoom(Event):
+    """ """
+
     resource = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
-class EvenBus(Event):
-    """
-    """
+class EventBus(Event):
+    """ """
+
     resource = models.ForeignKey(Bus, on_delete=models.CASCADE)
     available_seats = models.IntegerField(validators=[MinValueValidator(0)])
-    departure  = models.CharField(max_length=64)
-    destination  = models.CharField(max_length=64)
+    departure = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
 
-
     def save(self, *args, **kwargs):
         """
-        Initialization of the number of available seats at the creation 
+        Initialization of the number of available seats at the creation
         and first save of the event.
         """
-        if self.pk is None and self.available_seats is None: 
-            self.available_seats = self.resource.number_seats  
+        if self.pk is None and self.available_seats is None:
+            self.available_seats = self.resource.number_seats
         super().save(*args, **kwargs)
 
-class EvenMaterial(Event):
-    """
-    """
+
+class EventMaterial(Event):
+    """ """
+
     resource = models.ForeignKey(Material, on_delete=models.CASCADE)
     available_stock = models.IntegerField(validators=[MinValueValidator(0)])
 
     def save(self, *args, **kwargs):
         """
-        Initialization of the available stock at the creation 
+        Initialization of the available stock at the creation
         and first save of the event.
         """
-        if self.pk is None and self.available_stock  is None: 
-            self.available_stock = self.resource.stock 
+        if self.pk is None and self.available_stock is None:
+            self.available_stock = self.resource.stock
         super().save(*args, **kwargs)
-
-
---
+++ src/backend/reservations/models.py
@@ -0,0 +1,83 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.db import models
+from evenements.models import EventBus, EventMaterial, EventRoom
+
+
+class Reservation(models.Model):
+    """
+    Classe mère pour toutes les réservations
+    """
+
+    id = models.AutoField(primary_key=True)
+    consumer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class ReservationBus(Reservation):
+    """
+    Réservation d'un bus (plusieurs utilisateurs peuvent réserver le même bus)
+    """
+
+    event = models.ForeignKey(EventBus, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        """
+        vérifie si des places sont disponibles avant d'enregistrer la réservation.
+        Si des places sont disponibles, elle réduit le nombre de places et sauvegarde
+        """
+        if self.event.available_seats > 0:
+            self.event.available_seats -= 1
+            self.event.save()
+            super().save(*args, **kwargs)
+        else:
+            raise ValueError("Plus de places disponibles")
+
+
+class ReservationRoom(Reservation):
+    """
+    Réservation d'une salle(une seule réservation possible par créneau)
+    """
+
+    event = models.ForeignKey(EventRoom, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        """
+        Contrainte d'unicité
+        """
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["event", "date", "start_time", "end_time"],
+                name="unique_reservation_room",
+            ),
+        ]
+
+
+class ReservationMaterial(Reservation):
+    """
+    Réservation de matériel
+    """
+
+    event = models.ForeignKey(EventMaterial, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+
+    def save(self, *args, **kwargs):
+        """
+        vérifie si le stock disponible est suffisant avant d'enregistrer la réservation
+        Si la quantité demandée est disponible, elle réduit le stock et sauvegarde
+        """
+        if self.event.available_stock >= self.quantity:
+            self.event.available_stock -= 1
+            self.event.save()
+            super().save(*args, **kwargs)
+        else:
+            raise ValueError("Stock insuffisant pour cette réservation")
