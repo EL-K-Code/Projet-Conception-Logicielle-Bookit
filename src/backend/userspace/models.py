e1701b2fcfbbe6e0e20c79b62a12aa42ec8f5ea3
Yatoute <mintoamayatoute@gmail.com>
2025-02-14 18:53:17 +0100

    Ajout d'un serializer pour le modèle User

--- src/backend/userspace/models.py
+++ src/backend/userspace/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.core.validators import EmailValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -6,6 +6,17 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     """Modèle utilisateur"""
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_("groups"),
+        blank=False,
+        null=False,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+    )
 
     email = models.EmailField(
         _("email address"),
--- src/backend/userspace/signals.py
+++ src/backend/userspace/signals.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from django.contrib.auth.models import Group
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
 from userspace.models import User
 
@@ -10,23 +10,36 @@ logger = logging.getLogger("django")
 
 
 @receiver(post_migrate)
+def create_default_groups(sender, **kwargs):
+    """Créer les groupes d’utilisateurs après la migration."""
+    if sender.name == "userspacue":
+        groups = ["admin", "event_admin", "consumer"]
+        for group_name in groups:
+            Group.objects.get_or_create(name=group_name)
+
+
+@receiver(post_migrate)
 def create_default_superuser(sender, **kwargs):
     """Créer un super utilisateur après la migration."""
     if sender.name == "userspace":
         try:
-            User.objects.create_superuser(
+            # Création du superutilisateur
+            superuser = User.objects.create_superuser(
                 username=os.getenv("DJANGO_SUPERUSER_USERNAME", default="admin"),
                 email=os.getenv("DJANGO_SUPERUSER_EMAIL", default="admin@gmail.com"),
                 password=os.getenv("DJANGO_SUPERUSER_PASSWORD", default="admin"),
             )
+
+            # Ajout du superutilisateur au groupe "admin"
+            admin_group, _ = Group.objects.get_or_create(name="admin")
+            superuser.groups.add(admin_group)
+
         except Exception as e:
             logger.error(f"Une erreur est survenue : {e}", exc_info=True)
 
 
-@receiver(post_migrate)
-def create_default_groups(sender, **kwargs):
-    """Créer les groupes d’utilisateurs après la migration."""
-    if sender.name == "userspace":
-        groups = ["event_admin", "consommateur"]
-        for group_name in groups:
-            Group.objects.get_or_create(name=group_name)
+@receiver(post_save, sender=User)
+def add_default_group(sender, instance, created, **kwargs):
+    """Assigner un groupe par défaut à chaque inscription d'une utilisateur"""
+    if created and not instance.groups.exists():
+        instance.groups.set([Group.objects.get(name="consumer")])
