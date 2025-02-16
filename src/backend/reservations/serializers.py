5af19b836a38f0e08516d76c0be3f171ff13bc7d
Richard GOZAN <gozamegborichard14@gmail.com>
2025-02-16 16:26:30 +0100

    Ajout des serializers dans l'app reservations

---
+++ src/backend/reservations/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import ReservationBus, ReservationMaterial, ReservationRoom
+
+
+class ReservationBusSerializer(serializers.ModelSerializer):
+    """
+    Classe Serializer pour le model ReservationBus
+    """
+
+    class Meta:
+        model = ReservationBus
+        fields = "__all__"
+
+
+class ReservationRoomSerializer(serializers.ModelSerializer):
+    """
+    Classe Serializer pour le model ReservationRoom
+    """
+
+    class Meta:
+        model: ReservationRoom
+        fields = "__all__"
+
+
+class ReservationMaterialSerializer(serializers.ModelSerializer):
+    """
+    Classe Serializer pour le model ReservationMaterial
+    """
+
+    class Meta:
+        model: ReservationMaterial
+        fields = "__all__"
