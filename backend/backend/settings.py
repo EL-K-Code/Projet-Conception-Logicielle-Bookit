4209dc441f46503e2467370e36be8c38b478ae1a
Yatoute <mintoamayatoute@gmail.com>
2025-03-14 08:07:50 +0100

    Revue du code et Déployment de l'application avec kubenete

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default="")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", default=False).lower() == "true"
+DEBUG = os.getenv("DJANGO_DEBUG", default="False") == "true"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default="").split(",")
 
